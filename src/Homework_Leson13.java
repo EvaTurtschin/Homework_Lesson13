0532864a85018e41d2995992a36be3a3a4369976
AIT TR Student <evgeniya@turtschin@gmail.com>
2023-11-27 15:21:08 +0100

    new tasks have done

---
+++ src/Homework_Leson13.java
@@ -0,0 +1,50 @@
+public class Homework_Leson13
+{
+    public static void main(String[] args)
+    {
+        /*Task 1
+-Cоздать массив строк fruits (фрукты) из 4 элементов: Orange,Apple,Banana,Mango.
+-Вывести созданный массив на экран используя цикл for.
+-Заменить в созданном массиве значение первого элемента на Kiwi
+-Вывести измененный массив на экран используя цикл for.
+Задание можно выполнить непосредственно в методе main, не создавая отдельный метод.*/
+
+        String[] fruits = {"Orange", "Apple", "Banana", "Mango"};
+        int a = 0;
+        for (a=0; a< fruits.length; a++)
+        {
+            System.out.println((a+1) + " fruit is " + fruits[a]);
+        }
+        System.out.println("\nThe new list of fruits:");
+        fruits[0] = "Kiwi";
+        for (a=0; a< fruits.length; a++)
+        {
+            System.out.println((a+1) + " fruit is " + fruits[a]);
+        }
+        System.out.println(" ");
+
+        /*Task 2
+Используя цикл for вывести на экран все четные элементы массива fruits из первой задачи.
+Задание можно выполнить непосредственно в методе main либо создать отдельный метод.*/
+
+        for (a=0; a< fruits.length;a=a+2)
+        {
+            System.out.println(fruits[a+1]);
+
+        }
+        System.out.println(" ");
+
+        /*Task 3
+Дан массив целых чисел. Написать метод, возвращающий наименьший элемент массива.
+public static int findMin(int[] ints)   */
+
+        int [] numbers = {25, 7, 2, 13, 34, 49};
+        int min = numbers[0];
+        for(int b=0; b<numbers.length; b++)
+        {
+            if (numbers[b]<min)
+                min = numbers[b];
+        }
+        System.out.println("Minimal number of this array is " + min);
+    }
+}
